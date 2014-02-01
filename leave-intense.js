c006e4b06cf75460ce39c471e365f9413185b6d9
chirag <chirag@viralmint.com>
2014-02-01 17:40:57 +0530

    init lib

---
+++ leave-intense.js
@@ -0,0 +1,43 @@
+document.LeaveIntense = function (options) {
+
+    Helper = {
+
+        /**
+         * Recursively merge two objects
+         * @param {Object} targetObject
+         * @param {Object} sourceObject
+         */
+        mergeObject: function (targetObject, sourceObject) {
+            for (var property in sourceObject) {
+                try {
+                    targetObject[property] = (sourceObject[property].constructor == Object) ? this.mergeObject(targetObject[property], sourceObject[property]) : sourceObject[property];
+                } catch (e) {
+                    targetObject[property] = sourceObject[property];
+                }
+            }
+            return targetObject;
+        }
+    }
+
+    var default_options = {
+        height: 10,                         // top height (y axis) to detect when user is abut to leave, value must be integer
+        beforeLeave: function (result) {}   // a callback function, the function will be called before user tries to leave window
+    };
+
+    options = Helper.mergeObject(default_options, options);
+
+    /*
+     * bind mouse move event and find y for current cursor
+     * it also calculates difference if page has scroll bar
+     */
+    document.onmousemove = function (event) {
+        var y = event.pageY - window.pageYOffset;
+        var x = event.pageX - window.pageXOffset;
+        if (y <= options.height) {
+            options.beforeLeave({
+                pageY: y,
+                pageX: x
+            });
+        }
+    };
+};
